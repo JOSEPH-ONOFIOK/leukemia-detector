c7963550d74b133cc6e1e496857cec6e7b65f440
JOSEPH-ONOFIOK <josephonofiok08@gmail.com>
2025-07-05 18:45:32 -0700

    problems

--- model/predict.py
+++ model/predict.py
@@ -3,22 +3,23 @@ import numpy as np
 from PIL import Image
 from tensorflow.keras.losses import CategoricalCrossentropy
 
-# Load the trained model with custom_objects for CategoricalCrossentropy
+# Match the custom loss config from training
+custom_loss = CategoricalCrossentropy(label_smoothing=0.1)
+
+# Load model with matching custom loss
 model = tf.keras.models.load_model(
     "model/model.keras",
-    custom_objects={"CategoricalCrossentropy": CategoricalCrossentropy}
+    custom_objects={"CategoricalCrossentropy": custom_loss}
 )
 
-# Define class labels
+# Class labels
 classes = ['ALL', 'AML', 'CLL', 'CML', 'Healthy']
 
-# Preprocess the input image
 def preprocess_image(img):
     img = img.resize((224, 224))
     arr = tf.keras.preprocessing.image.img_to_array(img) / 255.0
     return np.expand_dims(arr, axis=0)
 
-# Make prediction
 def classify_image(image):
     input = preprocess_image(image)
     preds = model.predict(input)[0]
